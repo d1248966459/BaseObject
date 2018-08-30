1441393d7f32496b4897cc674c04f6dee4ed6a6d
Cocoa <cocoa@jiliguala.com>
2018-08-30 18:29:05 +0800

    * [Test] student

---
+++ BaseObjectTest/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  BaseObjectTest
+//
+//  Created by Cocoa on 2018/8/30.
+//  Copyright © 2018年 dcj. All rights reserved.
+//
+
+import UIKit
+
+class Student: BaseObject {
+
+}
